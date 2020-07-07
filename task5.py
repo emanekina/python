305dd545d4e7184c5944442c6119aeb96b605233
emanekina <emanekina@mail.ru>
2020-07-07 17:21:29 +0300

    исправленная версия

--- task5.py
+++ task5.py
@@ -1,15 +1,23 @@
-total_sum = 0
-print(f'Программа подсчитывает сумму введенных пользователем чисел.\nДля завершения программы и выхода из нее нужно ввести символ * и нажать Enter.')
-while True:
-    num_list = input('Укажите последовательность чисел через пробел').split()
-    print(num_list)
-    for el in num_list:
+def my_func(my_list):
+    sum_list = 0
+    for el in my_list:
         try:
-            total_sum = total_sum + float(el)
+            sum_list = sum_list + float(el)
         except ValueError:
-            print('Сумма всех введенных пользователем чисел равна ', total_sum)
-            print('Выполнение программы завершено')
-            break
-    print('Сумма введенных пользователем чисел равна ', total_sum)
-    continue
+            if el == '*':
+                return sum_list, False
+            else:
+                pass
+    return sum_list, True
 
+
+print(f'Программа подсчитывает сумму введенных пользователем чисел.\nДля завершения программы и выхода из нее нужно ввести символ * и нажать Enter.')
+total_sum = 0
+pointer = True
+while pointer:
+    num_list = input('Укажите последовательность чисел через пробел').split()
+    result_tuple = my_func(num_list)
+    total_sum = total_sum + result_tuple[0]
+    pointer = result_tuple[1]
+    print(f'{num_list}\nСумма чисел в введенной строке: {result_tuple[0]}\nСумма всех введенных чисел: {total_sum}')
+print('Программа завершена')
